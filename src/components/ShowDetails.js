0ead380bc814e4d4d0a09ab51c70c078072b44f8
Tanmay Rajput <rajput.tanmay16@gmail.com>
2022-03-12 02:09:20 +0530

    updates

--- src/components/ShowDetails.js
+++ src/components/ShowDetails.js
@@ -22,7 +22,10 @@ export default function ShowDetails(props) {
           <p className="show-text">Show Details</p>
         )}
         <button className="btn" onClick={showDetailHandler}>
-          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAM1JREFUSEvtlOENwiAQRl8nsCM4giO4iXUyHcURHMERdAPzJZA0pnAcreIP+EXCcS/f49qBRmtoxKWDf2a+q+6qv2bg74frCNwK4xfVliSegAtwBc4GXDWnUKd9clngEXgAu9AhB49Qlb6APfBMkS2w7h2C5hz8Eyrd9zWJ490c3A1V05LEObju602jXjNpbOYBL2mPffSmxVBv4lRyN7QWPE+uvStprer5oGrgtLLTu+ZzMv4Zdcfe4aqjLNzq4M1UWo26asvQZufNVL8BhbkiH/8ZzykAAAAASUVORK5CYII=" />
+          <img
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAM1JREFUSEvtlOENwiAQRl8nsCM4giO4iXUyHcURHMERdAPzJZA0pnAcreIP+EXCcS/f49qBRmtoxKWDf2a+q+6qv2bg74frCNwK4xfVliSegAtwBc4GXDWnUKd9clngEXgAu9AhB49Qlb6APfBMkS2w7h2C5hz8Eyrd9zWJ490c3A1V05LEObju602jXjNpbOYBL2mPffSmxVBv4lRyN7QWPE+uvStprer5oGrgtLLTu+ZzMv4Zdcfe4aqjLNzq4M1UWo26asvQZufNVL8BhbkiH/8ZzykAAAAASUVORK5CYII="
+            alt="images"
+          />
         </button>
       </div>
       {showDetails && <Details></Details>}
